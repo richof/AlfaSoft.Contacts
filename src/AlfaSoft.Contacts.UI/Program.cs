a8b5b537b50e2b630403038dab03485eacbf4361
Richar Milton Flores Flores <P38765@somoseducacao.com.br>
2022-12-27 17:25:54 -0300

    AccessControl

--- src/AlfaSoft.Contacts.UI/Program.cs
+++ src/AlfaSoft.Contacts.UI/Program.cs
@@ -2,13 +2,11 @@ using AlfaSoft.Contacts.UI.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using AlfaSoft.Contacts.DataAccess.Context;
-using AutoMapper;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using Pomelo.EntityFrameworkCore.MySql.Infrastructure.Internal;
-using Pomelo.EntityFrameworkCore.MySql.Internal;
+
 using AlfaSoft.Contacts.Business;
 using AlfaSoft.Contacts.DataAccess;
 using AlfaSoft.Contacts.Business.Interfaces.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,27 +32,10 @@ builder.Services.AddDbContext<MariaDbContext>(options => options
 //Services
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
 builder.Services.AddScoped<IContactService, ContactService>();
-builder.Services
-         .AddAuthentication()
-         .AddCookie(options =>
-         {
-             options.LoginPath = "/login";
-             options.LogoutPath = "/logout";
-         });
+
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseMigrationsEndPoint();
-//}
-//else
-//{
-//    app.UseExceptionHandler("/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
