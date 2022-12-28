dcaf8749bf473d39d10dcbe24ab072e7639e9b1a
Richar Flores <richoff@gmail.com>
2022-12-28 08:48:39 -0300

    Just removing email confirmation requirement for login

--- src/AlfaSoft.Contacts.UI/Program.cs
+++ src/AlfaSoft.Contacts.UI/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
              new MySqlServerVersion(new Version(10, 4, 6))));
 //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddRazorPages();
 
